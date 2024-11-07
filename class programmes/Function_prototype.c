4de9a977b6bba3367d7ef7a818ac49580426e747
Djkstra <sawantsukumar@gmail.com>
2024-11-07 17:40:18 +0530

    Create Function_prototype.c

---
+++ class programmes/Function_prototype.c
@@ -0,0 +1,31 @@
+/*
+	Name: Sukumar sawant
+	Date: 07-11-24 16:14
+	Description: Functions
+*/
+
+void display();
+
+#include<stdio.h>
+#include<conio.h>
+#include<math.h>
+
+int main(){
+	
+	int a= 10 ;
+	display(a);
+	//printf("%d",a);
+	
+	return 0 ;
+}
+void display(int a){
+
+	printf("%d\n",a);
+}
+
+
+
+
+
+
+
